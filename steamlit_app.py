37ccc56733a18d7cf39a74f00908d7da5b4946e5
dimsgsnow <138203302+dimsgsnow@users.noreply.github.com>
2023-06-30 12:14:11 +0300

    Update steamlit_app.py
    Change header and add icons + avocado toast item

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Khale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Eggs')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Khale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Eggs')
+streamit.text('🥑🍞 Hard-Boiled Free-Range Eggs')
