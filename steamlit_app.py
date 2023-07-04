eecb4abade5bf99b69873b1bfc4f60c74bc98bae
dimsgsnow <138203302+dimsgsnow@users.noreply.github.com>
2023-07-04 21:18:02 +0300

    Update steamlit_app.py
    Adding try catch for fruit selection

--- steamlit_app.py
+++ steamlit_app.py
@@ -27,19 +27,16 @@ streamlit.dataframe(fruites_to_show)
 # New Section to Display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json())
-
-
-# Normalize JSON output
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# out on screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
+try
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+       streamlit.error("Please select a fruit to get information")
+    else:
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+      streamlit.error()
 streamlit.stop()
 
 # setup connection to snowflake with requirements file
