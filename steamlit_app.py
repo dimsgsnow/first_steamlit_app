955e6026fe070b316da682888781e2e753b50b78
dimsgsnow <138203302+dimsgsnow@users.noreply.github.com>
2023-07-04 20:41:28 +0300

    Update steamlit_app.py
    fix fruit to add on line 55

--- steamlit_app.py
+++ steamlit_app.py
@@ -52,4 +52,4 @@ streamlit.dataframe(my_data_rows)
 streamlit.header("Add a fruits of your choice to the list!")
 
 fruit_to_add = streamlit.text_input('What fruit would you like to add?','Kiwi')
-my_cur.execute("insert into fruit_load_list values(fruit_to_add)")
+my_cur.execute("insert into fruit_load_list values("fruit_to_add")")
