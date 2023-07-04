07a0656f99323de2f733e0e16d003c4027b0f82c
dimsgsnow <138203302+dimsgsnow@users.noreply.github.com>
2023-07-04 21:41:55 +0300

    Update steamlit_app.py
    Add button for load list

--- steamlit_app.py
+++ steamlit_app.py
@@ -42,20 +42,25 @@ try:
         streamlit.dataframe(back_from_function)
 except URLError as e:
       streamlit.error()
+
+streamlit.header("Add a fruits of your choice to the list!")
+
+#snowflake related functions
+def get_fruit_load_list():
+        with my_cnx.cursor() as my_cur:
+                my_cur.execute("SELECT * from fruit_load_list")
+                return my_cur.fetchall()
+
+#add a button to load a fruit
+if streamlit.button('Get fruit load list'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_data_rows = get_fruit_load_list()
+        streamlit.dataframe(my_data_rows)
+
 streamlit.stop()
 
-# setup connection to snowflake with requirements file
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
 # User add fruit option box
-streamlit.header("Add a fruits of your choice to the list!")
+
 
 fruit_to_add = streamlit.text_input('What fruit would you like to add?','Kiwi')
 insert_query = 'insert into fruit_load_list values (\'' + fruit_to_add +'\')'
